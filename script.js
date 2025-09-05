43b6a056fe363ba061d1e38082dba9982bf33e36
mailtorrmonish-hash <mailtorrmonish@gmail.com>
2025-09-05 15:09:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JS functionality
+console.log("Welcome to My Fitness App!");
+
+// Example: show an alert on page load
+window.addEventListener('load', () => {
+    alert("Hello! Your Fitness App is ready!");
+});
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "my-app-cache-v1";
+const filesToCache = [
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
